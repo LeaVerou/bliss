f44b59a5f4180bef832eec33b728c86ec747e87e
Kim Røen <kim@kimroen.com>
2015-12-06 19:18:18 +0100

    Test the unique aspects of $.set

--- tests/DOM/SetSpec.js
+++ tests/DOM/SetSpec.js
@@ -13,4 +13,23 @@ describe("$.set", function() {
 		expect(element).to.exist;
 		expect(element.style.color).to.equal("red");
 	});
+
+	it("can be called on elements", function() {
+		var element = document.createElement("nav");
+		element._.set({className: "main-navigation"});
+
+		expect(element.className).to.equal("main-navigation");
+	});
+
+	it("sets other options as properties or attributes on the subject", function() {
+		var element = $.set(document.createElement("input"), {
+			id: "the-main-one",
+			type: "text",
+			disabled: true
+		});
+
+		expect(element.id).to.equal("the-main-one");
+		expect(element.disabled).to.be.true;
+		expect(element.type).to.equal("text");
+	});
 });
