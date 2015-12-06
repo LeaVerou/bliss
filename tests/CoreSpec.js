319e982a0529b7dfb8a3fd5c1bd21a9dfddefb90
Kim Røen <kim@kimroen.com>
2015-12-06 01:03:45 +0100

    Add some core smoke tests

--- tests/CoreSpec.js
+++ tests/CoreSpec.js
@@ -1,11 +1,10 @@
-
 describe("Core Bliss", function () {
   "use strict";
 
   beforeEach(function () {
-    fixture.setBase('tests/fixtures')
+    fixture.setBase('tests/fixtures');
     this.fixture = fixture.load('core.html');
-    document.body.innerHTML += this.fixture[0]
+    document.body.innerHTML += this.fixture[0];
   });
 
   // testing setup
@@ -13,5 +12,12 @@ describe("Core Bliss", function () {
     expect($('#fixture-container')).to.not.be.null;
   });
 
-});
+  it("has global methods and aliases", function() {
+    expect(Bliss).to.be.defined;
+    expect($).to.be.defined;
+    expect($$).to.be.defined;
 
+    expect($).to.equal(Bliss);
+    expect($$).to.equal($.$);
+  });
+});
