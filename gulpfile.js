c11023a22471d0f6ee88fcc02ff0a87a733aafb4
Joacim Löwgren <joacim.lowgren@gmail.com>
2015-12-07 09:31:23 +0100

    Add linting
    Docs asks contributors to following existing code style, but nothing is
    enforcing it, making it optional. This adds an eslint configuration and
    will exit the Gulp build if it encounters an error.

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,39 @@
 // grab our gulp packages
-var gulp  = require('gulp');
-var gutil = require('gulp-util');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename')
-var concat = require('gulp-concat');
+var gulp  = require("gulp");
+var gutil = require("gulp-util");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename")
+var concat = require("gulp-concat");
+var eslint = require("gulp-eslint");
 
-gulp.task('concat', function() {
-  return gulp.src(['bliss.shy.js', 'bliss._.js'])
-	.pipe(concat('bliss.js'))
-	.pipe(gulp.dest('.'));
+gulp.task("concat", function() {
+  return gulp.src(["bliss.shy.js", "bliss._.js"])
+	.pipe(concat("bliss.js"))
+	.pipe(gulp.dest("."));
 });
 
-gulp.task('minify', ['concat'], function() {
+gulp.task("minify", ["concat"], function() {
 	var u = uglify();
-	u.on('error', function(error){
+	u.on("error", function(error){
 		console.error(error);
 		u.end();
 	});
 
-	return gulp.src(['bliss.shy.js', 'bliss.js'])
+	return gulp.src(["bliss.shy.js", "bliss.js"])
 	.pipe(u)
-	.pipe(rename({ suffix: '.min' }))
-	.pipe(gulp.dest('.'))
-	
+	.pipe(rename({ suffix: ".min" }))
+	.pipe(gulp.dest("."))
 });
 
-gulp.task('watch', function() {
-	gulp.watch(["*.js"], ['concat', 'minify']);
+gulp.task("lint", function() {
+	return gulp.src(["bliss.shy.js", "bliss.js"])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
 });
 
-gulp.task('default', ['concat', 'minify']);
+gulp.task("watch", function() {
+	gulp.watch(["*.js"], ["lint", "concat", "minify"]);
+});
+
+gulp.task("default", ["lint", "concat", "minify"]);
