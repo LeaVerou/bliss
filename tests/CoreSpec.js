eb0cb618d2dba44dbb0556d283c39c13dd90b3fd
Allen Guo <guoguo12@gmail.com>
2015-12-08 09:07:13 -0800

    Check testing environment has appropriate features

--- tests/CoreSpec.js
+++ tests/CoreSpec.js
@@ -14,6 +14,11 @@ describe("Core Bliss", function () {
 		expect($('#fixture-container')).to.not.be.null;
 	});
 
+	it("is a valid testing environment", function () {
+		// PhantomJS fails this one by default
+		expect(Element.prototype.matches).to.not.be.undefined;
+	});
+
 	it("has global methods and aliases", function() {
 		expect(Bliss).to.exist;
 		expect($).to.exist;
