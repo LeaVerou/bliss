2bbc108aa11803a02eced010776433c774f3146f
Zachary Forrest y Salazar <zforrest@gmail.com>
2015-12-08 13:45:16 -0800

    #53 - Add tests for $$()

---
+++ tests/DOM/QuerySelectorAllSpec.js
@@ -0,0 +1,62 @@
+describe('$$() API', function() {
+
+  before(function() {
+    fixture.setBase('tests/fixtures');
+  });
+
+  beforeEach(function() {
+    this.fixture = fixture.load('querySelector.html');
+  });
+
+  it('returns a collection of one element when selecting an ID', function() {
+
+    var elements = $$('#fixture-container');
+
+    expect(elements).to.be.an.instanceOf(Array);
+    expect(elements.length).to.be.equal(1);
+    expect(elements[0].children.length).to.be.equal(2);
+
+	});
+
+  it('returns a collection for selecting elements with a class', function() {
+
+    var elements = $$('.list');
+
+    expect(elements).to.be.an.instanceOf(Array);
+    expect(elements.length).to.be.equal(2);
+
+  });
+
+  it('returns a collection for selecting elements without a context', function() {
+
+    var elements = $$('li');
+
+    expect(elements).to.be.an.instanceOf(Array);
+    expect(elements.length).to.be.equal(6);
+
+  });
+
+
+  it('returns a collection inside of a context', function() {
+
+    var elements = $$('li', $('.list'));
+
+    expect(elements).to.be.an.instanceOf(Array);
+    expect(elements.length).to.be.equal(3);
+
+  });
+
+  it('returns a collection that can be iterated over', function() {
+
+    var elements = $$('li');
+    var count = 0;
+
+    elements.forEach(function() {
+        count += 1;
+    });
+
+    expect(count).to.be.equal(6);
+
+  });
+
+});
