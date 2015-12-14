5dab5f7386796ec84da6422706b23c6ae5b346d7
William <wcastand@gmail.com>
2015-12-14 13:17:38 +0100

    fixing indentation issue

--- tests/events/ReadySpec.js
+++ tests/events/ReadySpec.js
@@ -2,8 +2,8 @@ describe('$.ready', function () {
 	it('exists', function () {
 		expect($.ready).to.exist;
 	});
+	// Should fire when DOM is ready
 	it('Test $.ready()', function (done) {
-		// Should fire when DOM is ready
 		var spy = sinon.spy();
 
 		$.ready().then(function(){
