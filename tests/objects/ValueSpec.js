a1962142e39b0d5cf638ae439a56316e43c38da9
Huzeifa.Ezzi <huzee007@gmail.com>
2015-12-19 23:50:45 +0530

    $.value tests

---
+++ tests/objects/ValueSpec.js
@@ -0,0 +1,32 @@
+describe("$.value", function() {
+
+	it("exists", function () {
+	    expect($.value).to.exist;
+	});
+
+	it("not throw",function(){
+	    expect($.value).not.to.throw(Error);
+	})
+
+	it("returns null if object is null", function() {
+		var obj = null;
+		expect($.value(obj)).and.to.be.null;
+	});
+
+	it("returns undefined if property not defined", function() {
+		var obj = {};
+		expect($.value(obj, "bar")).to.be.undefined;
+	});
+
+	it("returns value if property is defined", function() {
+		var obj = {
+			foo:1,
+			bar:{
+				bar1:"one" 
+			}
+		};
+		expect($.value(obj, "foo")).to.equal(1);
+		expect($.value(obj, "bar","bar1")).to.equal("one");
+	});
+
+});
