4429608f88dda7bff2a620f34844890f9e0166e1
Lea Verou <lea@verou.me>
2017-12-01 19:34:16 -0500

    Fix guloop (gulp loop ;), align gulpfile with ESLint rules

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,31 @@
 // grab our gulp packages
-var gulp  = require('gulp');
-var gutil = require('gulp-util');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename')
-var concat = require('gulp-concat');
+var gulp  = require("gulp");
+var gutil = require("gulp-util");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var concat = require("gulp-concat");
 
-gulp.task('concat', function() {
-  return gulp.src(['bliss.shy.js', 'bliss._.js'])
-	.pipe(concat('bliss.js'))
-	.pipe(gulp.dest('.'));
+gulp.task("concat", function() {
+  return gulp.src(["bliss.shy.js", "bliss._.js"])
+	.pipe(concat("bliss.js"))
+	.pipe(gulp.dest("."));
 });
 
-gulp.task('minify', ['concat'], function() {
+gulp.task("minify", ["concat"], function() {
 	var u = uglify();
-	u.on('error', function(error){
+	u.on("error", function(error) {
 		console.error(error);
 		u.end();
 	});
 
-	return gulp.src(['bliss.shy.js', 'bliss.js'])
+	return gulp.src(["bliss.shy.js", "bliss.js"])
 	.pipe(u)
-	.pipe(rename({ suffix: '.min' }))
-	.pipe(gulp.dest('.'))
-	
+	.pipe(rename({ suffix: ".min" }))
+	.pipe(gulp.dest("."));
 });
 
-gulp.task('watch', function() {
-	gulp.watch(["*.js"], ['concat', 'minify']);
+gulp.task("watch", function() {
+	gulp.watch(["*.js", "!bliss.js", "!*.min.js"], ["concat", "minify"]);
 });
 
-gulp.task('default', ['concat', 'minify']);
+gulp.task("default", ["concat", "minify"]);
