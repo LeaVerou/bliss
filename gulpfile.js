a91618fc2ae2cc37a9a4fb7c8028758d0c58f75b
Lea Verou <lea@verou.me>
2021-02-06 22:16:27 +0200

    Comment out build code since that's for v1

--- gulpfile.js
+++ gulpfile.js
@@ -5,27 +5,27 @@ var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
 var concat = require("gulp-concat");
 
-gulp.task("concat", function() {
-  return gulp.src(["bliss.shy.js", "bliss._.js"])
-	.pipe(concat("bliss.js"))
-	.pipe(gulp.dest("."));
-});
-
-gulp.task("minify", function() {
-	var u = uglify();
-	u.on("error", function(error) {
-		console.error(error);
-		u.end();
-	});
-
-	return gulp.src(["bliss.shy.js", "bliss.js"])
-	.pipe(u)
-	.pipe(rename({ suffix: ".min" }))
-	.pipe(gulp.dest("."));
-});
-
-gulp.task("watch", function() {
-	gulp.watch(["*.js", "!bliss.js", "!*.min.js"], gulp.series("concat", "minify"));
-});
-
-gulp.task("default", gulp.series("concat", "minify"));
+// gulp.task("concat", function() {
+//   return gulp.src(["bliss.shy.js", "bliss._.js"])
+// 	.pipe(concat("bliss.js"))
+// 	.pipe(gulp.dest("."));
+// });
+//
+// gulp.task("minify", function() {
+// 	var u = uglify();
+// 	u.on("error", function(error) {
+// 		console.error(error);
+// 		u.end();
+// 	});
+//
+// 	return gulp.src(["bliss.shy.js", "bliss.js"])
+// 	.pipe(u)
+// 	.pipe(rename({ suffix: ".min" }))
+// 	.pipe(gulp.dest("."));
+// });
+//
+// gulp.task("watch", function() {
+// 	gulp.watch(["*.js", "!bliss.js", "!*.min.js"], gulp.series("concat", "minify"));
+// });
+//
+// gulp.task("default", gulp.series("concat", "minify"));
